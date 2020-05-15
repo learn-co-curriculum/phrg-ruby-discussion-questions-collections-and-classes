dc0b658618ade17c841298c514668cf3d2988289
Stephen McKeon <stephen.mckeon@powerhrg.com>
2020-05-15 09:08:37 -0400

    Answers questions

--- review-question-1.rb
+++ review-question-1.rb
@@ -1,4 +1,6 @@
 ## QUESTION 1
+require "pry"
+
 
 pokemon = [
   {
@@ -62,8 +64,25 @@ pokemon = [
 
 
 # How would you get the url for Bulbasaur's ability?
+pokemon[0][:abilities][0][:ability][:url]
 # How would you return the first pokemon with base experience over 40?
+pokemon.find do |array|
+  array.each do |key, value|
+    key == :base_experience && value > 40
+  end
+end
 # How would you return ALL OF THE pokemon with base experience over 40? (Gotta catch em all)
+pokemon.select do |array|
+    array.each do |key, value|
+      key == :base_experience && value > 40
+    end
+  end
 # How would you return an array of all of the pokemon's names?
+pokemon.map do |array|
+  array[:name]
+end
 # How would you determine whether or not the pokemon array contained any pokemon with a weight greater than 60?
-#  whatever method you use should return true if there are any such pokemon, false if not.
+# whatever method you use should return true if there are any such pokemon, false if not.
+pokemon.any? do |array|
+  array[:weight] > 60
+end
